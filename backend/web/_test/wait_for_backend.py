80c1cf434643b96240012b0752e8a3101aaa04da
Fredrik Peteri <fredrik@peteri.se>
2021-02-10 17:59:45 +0100

    Catches a better exception in wait_for_backend.py

--- backend/web/_test/wait_for_backend.py
+++ backend/web/_test/wait_for_backend.py
@@ -8,9 +8,11 @@ base_url = f"https://localhost:25674"
 backend_up = False
 while not backend_up:
     try:
+        print(f"Trying to connect to backend at {base_url}")
         response = requests.get(base_url, verify=False)
         backend_up = response.status_code == 200
-    except Exception as e:
+    except requests.exceptions.RequestException as e:
+        print(str(e))
         backend_up = False
-    sleep(0.5)
+        sleep(1)
 
