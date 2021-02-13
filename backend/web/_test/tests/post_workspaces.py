811c75184aa0b17f75b0f6377724d55a89e4ac75
Fredrik Peteri <fredrik@peteri.se>
2021-02-13 09:29:53 +0100

    Adds post_workspaces canary test

---
+++ backend/web/_test/tests/post_workspaces.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+import requests
+import json
+
+api_base_url = f"https://localhost:25674/api/"
+
+data = {
+    "name": "Eden",
+    "info": "Lovely place, nice apples!"
+}
+
+try:
+    response = requests.post(api_base_url + "workspaces", verify=False, json=data)
+    if response.status_code != 200:
+        raise RuntimeError("Status code:", f"expected 200 got {response.status_code}") 
+    
+    exit(0)
+
+except RuntimeError as e:
+    print("\t" + f"{e}")
+    exit(1)
