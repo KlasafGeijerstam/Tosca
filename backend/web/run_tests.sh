d177759e99e17f7b9f2a22ee66b494827f5bccfa
Emilia Harr <emilia.harr@gmail.com>
2021-02-14 18:55:09 +0100

    Adds dependency check before running tests

--- backend/web/run_tests.sh
+++ backend/web/run_tests.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
+missing_dep=0
+dependencies=("pg_virtualenv" "cargo" "diesel" "openssl" "pkg-config")
+for dep in ${dependencies[*]}; do
+	hash $dep &> /dev/null || { printf "$dep is missing.\n"; missing_dep=1; }
+done
+
+if (( $missing_dep == 1 )) ; then
+	printf "Missing dependencies. Skipping tests.\n"
+	exit 1
+fi
+
 pg_virtualenv _test/all.sh
