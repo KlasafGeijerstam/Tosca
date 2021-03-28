9e9772bf8cd3ea2c464ad2ec79a4a6b0b131aa29
Fredrik Peteri <fredrik.peteri@gmail.com>
2021-03-28 09:41:22 +0200

    Fixes layout of workspaces with long descriptions
    - Workspaces will always show the default image if they
    have no specified image.
    - Workspaces description have a maximum height and will
    scroll vertically if needed.
    
    Closes #14

--- frontend/src/app/mock.ts
+++ frontend/src/app/mock.ts
@@ -6,7 +6,7 @@ export let workspaces = [
   {
     workspace_id: 1,
     name: 'Course In Science',
-    info: 'This is a course where we talk about science. What is science? Why is science valuable?\nPlease sign up to the waiting list for help during the lectures. Sign up to the seminar groups before 24/5.',
+    info: 'This is a course where we talk about science. What is science? Why is science valuable?\n\nPlease sign up to the waiting list for help during the lectures. Sign up to the seminar groups before 24/5.',
     img: 'assets/genericCardImg.png',
     creator: 'admin',
   },
@@ -37,6 +37,17 @@ export let workspaces = [
     info: 'Well, who knew this course would exist? I have not come up with a description yet.',
     img: 'assets/genericCardImg.png',
     creator: 'admin',
+  },
+  {
+    workspace_id: 6,
+    name: 'Lorem Ipsum',
+    info: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In non venenatis ipsum. Nam vitae mi imperdiet, interdum lorem vel, ornare velit. Praesent id sapien non metus interdum tincidunt nec quis metus. Sed hendrerit, nunc at euismod laoreet, nunc nulla convallis odio, at feugiat metus enim vitae est. Integer leo ex, condimentum eget molestie non, sagittis ac libero. Pellentesque ante mauris, suscipit tristique risus quis, laoreet tempus purus. Quisque quis eleifend sem. Fusce sodales libero eu magna placerat, sit amet imperdiet arcu dictum. Fusce lectus mi, consectetur et sem a, egestas fermentum nunc. Nulla tortor mi, interdum a consequat vel, iaculis id massa. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Proin ante metus, porttitor vel blandit ut, pellentesque ac nisi.\n\n' +
+      'Sed cursus neque leo, sed cursus est placerat id. Morbi vel mi ipsum. Suspendisse sagittis lectus dictum semper tincidunt. Aliquam tincidunt laoreet viverra. Morbi euismod pellentesque est sed placerat. Donec a venenatis ligula, vitae faucibus metus. Suspendisse sed feugiat lacus, vel viverra dolor. In tempor ipsum in luctus mattis.\n\n' +
+      'Vestibulum ac iaculis augue. Praesent a laoreet est. Nulla tempus, ligula quis faucibus blandit, dolor dui commodo purus, sit amet semper ipsum nisi vel tortor. Sed nec fermentum ex, nec dapibus velit. In hendrerit, metus non pharetra bibendum, ante lectus fermentum justo, ut aliquet lectus enim eget ipsum. Integer viverra in felis at porttitor. Vivamus vulputate ligula in lacus facilisis finibus. Praesent libero ligula, facilisis nec ultrices eget, vulputate eu arcu. Vivamus blandit pellentesque congue. Aenean enim nisl, sollicitudin a neque sed, mattis lacinia ligula.\n\n' +
+      'Donec ut mi rutrum, egestas ipsum ut, ullamcorper orci. Nam non orci facilisis, vulputate mauris a, convallis ligula. Ut elementum diam sed quam mattis convallis. Nam et lorem blandit, dignissim massa et, vestibulum nisl. Donec dictum interdum porttitor. Nullam sit amet ornare odio. Proin sagittis mi malesuada commodo placerat.\n\n' +
+      'Sed semper in magna eget egestas. Aliquam congue metus et tellus tincidunt volutpat. Phasellus tortor arcu, dictum sed vehicula eget, vestibulum sit amet erat. Sed quis lacus urna. Aliquam leo augue, aliquam mollis egestas a, tristique in eros. Sed consectetur vulputate justo, vitae venenatis nisi malesuada vel. Nulla vitae ligula nibh. Nunc efficitur ipsum vitae varius mattis. Duis finibus feugiat metus sed volutpat. Curabitur et felis ullamcorper, ullamcorper nulla sed, rutrum nunc. Cras lectus orci, gravida et luctus a, euismod sit amet tortor. Nunc ultricies molestie urna.',
+    img: '',
+    creator: 'super',
   }
 ];
 
