4c8413f092d7f250aee65e7b7cb202d65d1fcff9
Fredrik Peteri <fredrik.peteri@gmail.com>
2021-04-04 22:07:14 +0200

    Workspaces in GET workspaces contains queues
    - Cargo fmt
    
    Closes #21

--- backend/web/_test/tests/workspaces_test.py
+++ backend/web/_test/tests/workspaces_test.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 import sys
+import traceback 
 import requests
 
 API_BASE_URL = "https://localhost:25674/api/"
@@ -101,14 +102,79 @@ def invalid_token_post():
         raise AssertionError(f"Posting with invalid token, expected status 401 got {response.status_code}")
 
 
+def get_workspace_with_queues():
+    """
+    Create a workspace with two queues, check that we get back both queues when we request the workspace
+    """
+    data = {
+        "name": "get_workspace_with_queues",
+        "info": "Description"
+    }
+    response = requests.post(API_BASE_URL + "workspaces", headers=admin_headers, verify=False, json=data)
+    if response.status_code != 200:
+        raise AssertionError(f"Could not create workspace, got status {response.status_code}")
+    workspace_id = response.json()["id"]
+
+    queue1 = {
+        "workspace_id": workspace_id,
+        "name": "First Queue",
+        "info": "First Queue Info"
+    }
+    response = requests.post(API_BASE_URL + "queue", headers=admin_headers, verify=False, json=queue1)
+    if response.status_code != 200:
+        raise AssertionError(f"Could not create queue, got status {response.status_code}")
+    queue1["id"] = response.json()["id"]
+
+    queue2 = {
+        "workspace_id": workspace_id,
+        "name": "Second Queue",
+        "info": "Second Queue Info"
+    }
+    response = requests.post(API_BASE_URL + "queue", headers=admin_headers, verify=False, json=queue2)
+    if response.status_code != 200:
+        raise AssertionError(f"Could not create queue, got status {response.status_code}")
+    queue2["id"] = response.json()["id"]
+
+    response = requests.get(API_BASE_URL + "workspaces", headers=admin_headers, verify=False)
+    if response.status_code != 200:
+        raise AssertionError(f"Could not get workspaces, got status {response.status_code}")
+    workspace = [x for x in response.json() if x["id"] == workspace_id][0]
+
+    if len(workspace["queues"]) != 2:
+        raise AssertionError(f"Incorrect number of queues, expected 2, got {len(workspace['queues'])}")
+    
+    returned_queue1 = [q for q in workspace["queues"] if q["id"] == queue1["id"]][0]
+    if not returned_queue1:
+        raise AssertionError(f"Could not find queue1 in queues, got {workspace['queues']}")
+
+    if returned_queue1["name"] != queue1["name"]:
+        raise AssertionError( f"Invalid name for queue 1, got: \"{returned_queue1['name']}\", expected: \"{queue1['name']}\"")
+
+    if returned_queue1["info"] != queue1["info"]:
+        raise AssertionError( f"Invalid info for queue 1, got: \"{returned_queue1['info']}\", expected: \"{queue1['info']}\"")
+
+    returned_queue2 = [q for q in workspace["queues"] if q["id"] == queue2["id"]][0]
+    if not returned_queue2:
+        raise AssertionError(f"Could not find queue2 in queues, got {workspace['queues']}")
+
+    if returned_queue2["name"] != queue2["name"]:
+        raise AssertionError( f"Invalid name for queue 2, got: \"{returned_queue2['name']}\", expected: \"{queue2['name']}\"")
+
+    if returned_queue2["info"] != queue2["info"]:
+        raise AssertionError( f"Invalid info for queue 2, got: \"{returned_queue2['info']}\", expected: \"{queue2['info']}\"")
+
+
 try:
     get_empty_workspaces()
     admin_post()
     normal_post()
     super_post()
     invalid_token_post()
+    get_workspace_with_queues()
     sys.exit(0)
 
-except AssertionError as exception:
-    print(exception)
+except AssertionError:
+    _, exc_value, exc_traceback = sys.exc_info()
+    print(traceback.format_tb(exc_traceback, limit=1)[0].splitlines()[-1], end=": ")
+    print(exc_value)
     sys.exit(1)
