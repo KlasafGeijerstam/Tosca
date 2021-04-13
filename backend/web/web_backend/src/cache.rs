7459e0b182439bb78ed3901aa3966af71b7ef1aa
Jakub <kubajagiello94@gmail.com>
2021-04-13 08:14:00 +0200

    fixed comments and added builder. Cache has now fixed String type for keys.

--- backend/web/web_backend/src/cache.rs
+++ backend/web/web_backend/src/cache.rs
@@ -1,15 +1,15 @@
-use std::cmp::Eq;
 use std::collections::HashMap;
-use std::hash::Hash;
 use tokio::sync::RwLock;
 use std::sync::Arc;
+use std::time::Duration;
+use std::mem::replace;
 
 /// Put max size on cache.
 /// Put timeout on cache entries.
 
-struct MaxSize<T> {
+struct MaxSize {
     max_size: usize,
-    filo: Vec<T>,
+    filo: Vec<String>,
     index: usize
 }
 
@@ -18,7 +18,7 @@ struct MaxSize<T> {
 
 // When the queue hits maximum, we will start replacing the
 // oldest entries.
-impl<T: Clone> MaxSize<T> {
+impl MaxSize {
     fn new(max_size: usize) -> Self {
         MaxSize {
             max_size,
@@ -31,7 +31,7 @@ impl<T: Clone> MaxSize<T> {
 
     // If the cache is filled. Return a key to data that should be
     // removed
-    fn put(&mut self, data: T) -> Option<T> {
+    fn put(&mut self, data: String) -> Option<String> {
         // if the cache has not been filled yet
         if self.filo.len() < self.max_size {
             self.filo.push(data);
@@ -39,67 +39,94 @@ impl<T: Clone> MaxSize<T> {
             None
         } else {
             // If we are at the last index, set index to start position
-            if self.is_max() {
+            if self.has_reached_max_size() {
                 self.index = 0;
             }
-            
-            let ret = self.filo.get(self.index).unwrap().clone();
-            self.filo[self.index] = data;
+
+            let ret = replace(&mut self.filo[self.index], data);
             self.index += 1;
             Some(ret)
         }
     }
 
-    fn is_max(&self) -> bool {
+    fn has_reached_max_size(&self) -> bool {
         self.filo.len() == self.max_size
     }
 
 }
 
-
-
-pub struct Cache<K, V> {
-    cache: RwLock<HashMap<K, Arc<V>>>,
-    max_size: Option<RwLock<MaxSize<K>>>,
-    timeout: Option<u32>,
+pub struct CacheBuilder {
+    max_size: Option<usize>,
+    timeout: Option<Duration>,
 }
 
-impl<K: Eq + Hash + Clone, V: Clone> Cache<K, V> {
+impl CacheBuilder {
+    
     pub fn new() -> Self {
-        Cache {
-            cache: RwLock::new(HashMap::new()),
+        CacheBuilder {
             timeout: None,
             max_size: None,
         }
     }
 
     pub fn with_max_size(self, max_size: usize) -> Self {
-        Cache {
-            cache: self.cache,
-            max_size: Some(RwLock::new(MaxSize::new(max_size))),
+        CacheBuilder {
+            max_size: Some(max_size),
             timeout: self.timeout
         }
     }
 
-    pub fn with_timeout(self, timeout: u32) -> Self {
+    pub fn with_timeout(self, __timeout: Duration) -> Self {
         unimplemented!();
     }
 
-    pub async fn lookup(&self, key: &K) -> Option<Arc<V>> {
+    pub fn build<V>(self) -> Cache<V> {
+        Cache {
+            cache: RwLock::new(HashMap::new()),
+            __timeout: self.timeout,
+            max_size: self.max_size.map(|size| RwLock::new(MaxSize::new(size))),
+        }
+    }
+}
+
+impl Default for CacheBuilder {
+    fn default() -> Self {
+        CacheBuilder::new()
+    }
+}
+
+pub struct Cache<V> {
+    cache: RwLock<HashMap<String, Arc<V>>>,
+    max_size: Option<RwLock<MaxSize>>,
+    __timeout: Option<Duration>,
+}
+
+impl<V: Clone> Cache<V> {
+
+    pub fn builder() -> CacheBuilder {
+        CacheBuilder::default()
+    }
+
+    pub async fn lookup(&self, key: &str) -> Option<Arc<V>> {
         self.cache.read().await.get(key).cloned()
     }
 
     // Save key and value in cache
-    pub async fn store(&self, key: &K, value: V) {
-        self.cache.write().await.insert(key.clone(), Arc::new(value.clone()));
+    pub async fn store(&self, key: &str, value: V) -> Arc<V> {
+        let value = Arc::new(value);
+        self.cache.write().await.insert(String::from(key), value.clone());
 
         // If the max_size is set
         if let Some(max_size) = &self.max_size {
             // if we have filled the buffer
-            if let Some(to_remove) = max_size.write().await.put(key.clone()) {
+            if let Some(to_remove) = max_size.write().await.put(String::from(key)) {
                 // remove the least recent
                 self.cache.write().await.remove(&to_remove);
             }
         }
+        value.clone()
     }
 }
+
+// Write tests
+// Benchmark
